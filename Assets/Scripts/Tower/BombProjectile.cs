b38928ffa718f4b5e62ac28834af51326a984f12
duongp2c <85700572+duongp2c@users.noreply.github.com>
2023-03-20 21:05:31 +0700

    fix projectile

--- Assets/Scripts/Tower/BombProjectile.cs
+++ Assets/Scripts/Tower/BombProjectile.cs
@@ -2,5 +2,13 @@
 
 public class BombProjectile : Projectile
     {
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        Enemy newP = collider2D.gameObject.GetComponent<Enemy>();
+        if (collider2D.tag == "Enemy")
+        {
+            newP.takeDamage(attackDamage);
+        }
     }
+}
 
